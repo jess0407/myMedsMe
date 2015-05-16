33d5e9648b7c4205e2c43ef79e2edb07f5ce75e3
Jessie Shi <jessie.shi@me.com>
2015-05-17 00:01:56 +0100

    Display data nicely also deal with corrupted json and duplicated data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,19 +11,52 @@ angular.module('myMedsMeApp')
   .controller('MainCtrl', function ($scope, dataService, $interval) {
 
       $scope.dataset = {
-          medicalData: []
+          imaging: [],
+          medications: [],
+          labs: []
       };
 
       dataService.socket.onmessage = function (event) {
           displayData(event.data);
       };
 
-      var displayData = function ( data ) {
-          // if data does not exisit in the medicalData array, add to it.
-          if( $scope.dataset.medicalData.indexOf(data) === -1){
-              $scope.dataset.medicalData.push(data);
+      // To test if an obj is already in an array to handle duplicate objects
+      function isObjInArr (obj, arr) {
+
+          for(var i = 0; i < arr.length ; i++) {
+              if (angular.equals(obj, arr[i])) {
+                  return true;
+              }
           }
-      };
+
+          return false;
+      }
+
+
+      function displayData ( data ) {
+          var json;
+
+          try {
+               json = JSON.parse(data);
+          } catch (e) {
+              console.log(e);
+              return;
+          }
+
+          // if data does not exisit in the medicalData array, add to it.
+          if( json.imaging  && !isObjInArr(json, $scope.dataset.imaging) ) {
+              $scope.dataset.imaging.push(json);
+          }
+
+          if( json.medications && !isObjInArr(json, $scope.dataset.medications) ){
+              $scope.dataset.medications.push(json);
+          }
+
+          if( json.labs && !isObjInArr(json, $scope.dataset.labs) ){
+              $scope.dataset.labs.push(json);
+          }
+
+      }
 
       // update $scope every 3 seconds
       $interval(function(){},3000);
